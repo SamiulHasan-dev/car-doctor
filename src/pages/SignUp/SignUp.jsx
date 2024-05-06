ea7c27b9bc3996e7b320b46c62e81a50a6cee0a1
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-06 16:29:27 +0600

    work on authProvider

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,16 +1,34 @@
 import { Link } from "react-router-dom";
 import img from '../../assets/images/login/login.svg'
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const SignUp = () => {
+    const {createUser} = useContext(AuthContext);
 
     const handleSignUp = event =>{
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(name,email,password);
+
+        createUser(email, password)
+        .then(result=>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
     }
 
 
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero min-h-screen ">
   <div className="hero-content flex-col lg:flex-row md:gap-10">
     <div className="">
       <img src={img} alt="" />
