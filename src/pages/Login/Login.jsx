bf78d7839a611f26b3fe08922577893ada59a838
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-18 20:41:32 +0600

    jwt payload in login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import axios from 'axios';
 
 const Login = () => {
     const {signIn} = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     const handleLogin = event =>{
         event.preventDefault();
@@ -14,9 +17,18 @@ const Login = () => {
 
         signIn(email, password)
         .then(result =>{
-          const user = result.user;
-          console.log(user);
+          const loggedInUser = result.user;
+          console.log(loggedInUser);
+          const user = { email };
+          // navigate(location?.state ? location?.state : '/')
+          //get access token
+          axios.post('http:// localhost:5000/jwt', user)
+          .then(res => {
+            console.log(res.data);
+          })
+
         })
+        
         .catch(error=>{
           console.log(error)
         })
